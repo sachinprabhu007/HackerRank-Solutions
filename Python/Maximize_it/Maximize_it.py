15701770f705392dfd70e9f33d51fe747b5a89ff
Sachin Prabhu <sachintheprecious@gmail.com>
2019-02-21 22:22:29 +0530

    Add files via upload

--- Python/Maximize_it/Maximize_it.py
+++ Python/Maximize_it/Maximize_it.py
@@ -1,3 +1,8 @@
-K,M = map(int,input().split())
-print (sum([max(list(map(int,input().strip().split()[1:])))**2 for _ in range(K)])%M)
+#K,M = map(int,input().split())
+#print (sum([max(list(map(int,input().strip().split()[1:])))**2 for _ in range(K)])%M)
 
+import itertools
+
+k, m = map(int, input().split())
+
+print(max(sum(j) % m for j in itertools.product(*((int(i) ** 2 for i in input().split()[1:]) for _ in range(k)))))
